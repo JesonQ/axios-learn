b08adc2b64ed749dd41667784de1753b53c73fcd
“gihzq <614977099@qq.com>
2020-07-22 14:25:54 +0800

    l

--- 3-AJAX-API/server.js
+++ 3-AJAX-API/server.js
@@ -14,7 +14,6 @@ app.all("/server", (request, response) => {
     //返回 data.json 的文件数据
     // response.end("服务器端的数据");
     response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader('Access-Control-Allow-Headers', '*')
     response.end('{"data": "服务器端的数据"}');
 });
 
